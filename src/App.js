2c246fb4cd11fe146a7d6df1b99c97bec370d01b
iYowna <hanoeldrinp@gmail.com>
2024-10-27 18:00:41 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,4 @@ menuIcon.onclick = () => {
   navbar.classList.toggle('active');
 };
  
-const sr = ScrollReveal({
-  distance: '65px',
-  duration: 2600,
-  delay: 450,
-  reset: true
-});
-
-sr.reveal('.home-content',{delay:200,origin:'top'});
-sr.reveal('.home-img',{delay:450,origin:'top'});
-
-
-
+ 
